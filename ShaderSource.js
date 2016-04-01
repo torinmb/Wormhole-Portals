5004fcbe689a8367e983899ee8fa97d36d4f6206
TehSAUCE <peterwhidden@gmillerokpro152.localdomain>
2016-04-01 09:29:22 -0700

    Warping sort of tracks its "origin" but not very well yet

--- ShaderSource.js
+++ ShaderSource.js
@@ -6,14 +6,19 @@ function vertex() {
         'precision highp float;',
         'varying vec2 vUv;',
         'varying vec2 WHlocation;',
+        'varying vec3 pos;',
         'uniform vec3 wormholePos;',
         'uniform vec2 resolution;',
+        'uniform mat4 originalProjMatrix;',
+        'uniform mat4 originalModelMatrix;',
+
 
         'void main() {',
 
+    //    'pos = position * 2.0;',
         '  vUv = uv;',
-        'vec4 temp = projectionMatrix * modelViewMatrix * vec4( wormholePos, 1.0 );',
-        '  WHlocation = (temp.xy / (temp.z*temp.w)) * resolution + 0.5*resolution;',
+        'vec4 temp = originalProjMatrix * originalModelMatrix * vec4( wormholePos, 1.0 );',
+        '  WHlocation = (temp.xy + 0.5) * resolution;',
         '  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
 
         '}'
@@ -32,13 +37,14 @@ function fragPassTwo() {
         //'precision highp float;',
         'varying vec2 vUv;',
         'varying vec2 WHlocation;',
+        'varying vec3 pos;',
         'uniform sampler2D firstPass;',
 
         'void main() {',
 
         'mediump vec2 test = vec2(0.0,0.0);',
        // 'vec2 warp = normalize(WHlocation - gl_FragCoord) * pow(distance(WHlocation, gl_FragCoord), -2.0) * 30.0;',
-        'vec2 warp = normalize(WHlocation - gl_FragCoord.xy) * pow(distance(WHlocation, gl_FragCoord.xy), -2.0) * 500.0;',
+        'vec2 warp = normalize(WHlocation - gl_FragCoord.xy) * pow(distance(WHlocation, gl_FragCoord.xy), -2.0) * 2500.0;',
         // warp.y = -warp.y;
 
         '   gl_FragColor = texture2D( firstPass, (vUv + warp));',
